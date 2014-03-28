72533d361d68db3a6b51a8cb6db2ee69b7ae1f9c
SkyChang <sancsky@livemail.tw>
2014-03-29 00:31:58 +0800

    簽入

---
+++ Lottery/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lottery.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            Random r = new Random();
+            ViewData["no"] = r.Next(60);
+            return View();
+        }
+	}
+}
